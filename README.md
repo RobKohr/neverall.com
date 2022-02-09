f744913a667b3d8c023cdfd2b1a13070861f3322
Rob Kohr <spam@robkohr.com>
2022-02-08 18:05:01 -0800

    Add some copy, style links

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ how to setup interactive sites.
 ![](2021-07-08-14-53-35.png)
 
 Use hotjar for tracking
+
+geomanist font
--- src/sites/feedback/pages/Home.tsx
+++ src/sites/feedback/pages/Home.tsx
@@ -3,6 +3,7 @@ import Button from "components/forms/Button";
 import Lipsum from "components/Lipsum";
 import PlaceholderImage from "components/PlaceholderImage";
 import React, { ReactNode } from "react";
+import { ReactComponent as NeverallLogo } from "../../../assets/icons/neverall-logo.svg";
 
 export default function Home() {
   return (
@@ -10,7 +11,26 @@ export default function Home() {
       <Banner>
         <div style={{ padding: "2rem", flexGrow: 1 }}>
           <h2>Get Feedback from your customers</h2>
-          <p>This is some powerful convincing text</p>
+          <p>
+            Feedback Forums by{" "}
+            <a href="https://neverall.com">
+              <NeverallLogo
+                style={{
+                  width: "3em",
+                  height: "3em",
+                  fill: "blue",
+                  margin: "-3px",
+                }}
+                viewBox="0 0 100 30"
+              />
+              NeverAll
+            </a>{" "}
+            empowers your users to provide ideas, suggestions, and report bugs.
+          </p>
+          <p>
+            Beyond all that, it also allows users to vote up or down feedback to
+            help you prioritize what really matters most to users.
+          </p>
           <p>
             <Button to="create" isActionButton>
               Create A Feedback Forum
